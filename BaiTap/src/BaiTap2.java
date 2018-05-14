4463ddcbc1e5b4c6642950bbf50b1d126c3c04a5
ThanhIT <ThanhIT@DESKTOP-8HVRLSR>
2018-05-14 13:10:12 +0700

    Bài Tập

--- BaiTap/src/BaiTap2.java
+++ BaiTap/src/BaiTap2.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 public class BaiTap2{
 	
 	private static void baitoan2() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Nhap vao so phan tu n");
+		int n = sc.nextInt();
+		for (int i= 0; i<n; i++)
+		{
+			for( int j= 0; j<=i; j++)
+			{
+				System.out.printf("* ");	
+			}
+			  System.out.println("");
+		}
 		
+	    for (int i= n; i>0; i--) {
 		
+			for(int k= i; k>0; k--)
+			{
+				System.out.printf("* ");
+		    }
+			System.out.println("");
+			
+		}	
 	}
   
 
@@ -12,30 +31,7 @@ public class BaiTap2{
 		// TODO Auto-generated method stub
 			System.out.println("Kết quả bài toán 2 là: ");
 			baitoan2();
-			Scanner sc= new Scanner(System.in);
-			System.out.println("Nhap vao so phan tu n");
-			int n = sc.nextInt();
-			for (int i= 0; i<n; i++)
-			{
-				for( int j= 0; j<=i; j++)
-				{
-					System.out.printf("* ");	
-				}
-				  System.out.println("");
-			}
-			
-		    for (int i= n; i>0; i--) {
-			
-				for(int k= i; k>0; k--)
-				{
-					System.out.printf("* ");
-			    }
-				System.out.println("");
-				
-			}
-			
-			
-			
+	
 	}
 
 }
