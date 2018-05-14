e3ed502810ce844a0a39a760428df4ca1918c44d
ThanhIT <ThanhIT@DESKTOP-8HVRLSR>
2018-05-14 13:36:49 +0700

    Bài Tập

---
+++ BaiTap/src/BaiTap3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BaiTap3 {
+	 
+	 public static void Nhap() {
+		 String s;
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Hãy nhập vào chuỗi kí tự");
+		 s= sc.nextLine();
+		 System.out.println("Hãy nhập kí tự bạn cần đếm :");
+		 char c;
+		  c= sc.next().charAt(0);
+		  int count = 0;
+		  
+		  for( int i = 0; i<s.length(); i ++)
+		  {
+			  if(s.charAt(i) == c)
+			  
+				  count++;
+				 
+			
+		  }
+		  if(count == 0)
+			  System.out.println("Kí tự "+ c + " không có trong chuỗi");
+		  else
+			  System.out.println("Số lần xuất hiện của kí tự "+ c + " là: " + count+ " lần");
+		 
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kết quả của bài tập 3 là: ");
+		Nhap();
+
+	}
+
+}
