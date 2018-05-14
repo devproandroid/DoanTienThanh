c193e0445c6ab665404ce024ab2b6544cff995ec
ThanhIT <ThanhIT@DESKTOP-8HVRLSR>
2018-05-14 13:04:06 +0700

    Bài Tập

--- BaiTap/src/BaiTap1.java
+++ BaiTap/src/BaiTap1.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class BaiTap1 {
+	
+	private static void baitoan() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Nhap vao so phan tu n");
+		int n = sc.nextInt();
+		for (int i= 0; i<n; i++)
+		{
+			for( int j= 0; j<=i; j++)
+			{
+				System.out.printf("* ");
+				
+				
+			}
+			System.out.println("");
+			
+		}
+		
+		
+	}
+  
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Thanh");
+			System.out.println("Kết quả bài toán 1 là: ");
+			baitoan();
+			
+			
 	}
 
 }
